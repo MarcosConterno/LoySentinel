295abcba5abb767a98c9c3a442dfe46f6a8483a1
Marcos Vincius <mvcs1996@hotmail.com>
2026-02-12 18:29:43 -0300

    correto

--- debug-scan.js
+++ debug-scan.js
@@ -3,5 +3,7 @@ const { runPass } = require("./src/scan");
 (async () => {
   const url = "https://prod.gestorjuridico.com.br/Paginas/Principal/_FSet_Abertura.asp";
   const r = await runPass({ url, headless: false });
+
+  // Vai mostrar typing + clickTest com antes/depois
   console.log(JSON.stringify(r.states?.[0]?.typing, null, 2));
 })();
